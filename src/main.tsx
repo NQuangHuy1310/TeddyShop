843c4fc1b611e3df2a13a77469b599a1bcac85a8
Nguyen Quang Huy <kaisa13102004@gmail.com>
2024-02-23 00:33:18 +0700

    redux-setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import GlobalStyles from '~/components/GlobalStyles'
+import { store } from './app/store.ts'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalStyles>
-      <App />
-    </GlobalStyles>
+    <Provider store={store}>
+      <GlobalStyles>
+        <App />
+      </GlobalStyles>
+    </Provider>
   </React.StrictMode>
 )
